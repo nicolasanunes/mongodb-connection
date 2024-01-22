4618ee6f3bb19bcb1ad49e352eaffedef46c515f
Nícolas Nunes <nicolasanunes@hotmail.com>
2024-01-22 17:26:58 -0300

    db.config.service.ts updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DbConfigService } from './db/db.config';
+import { DbConfigService } from './db/db.config.service';
 
 @Module({
   imports: [
